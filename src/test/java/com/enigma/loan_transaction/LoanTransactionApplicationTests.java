8060b0ea5dc8853af75f33b0b9be4352b25015d4
Dita Salsabila <ditsabil@gmail.com>
2024-03-21 13:06:29 +0700

    Initial commit

---
+++ src/test/java/com/enigma/loan_transaction/LoanTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.loan_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
